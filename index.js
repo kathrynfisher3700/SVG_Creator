12588efeaae36d933d39c1ec7ff03966280fcb69
Aprecity <kathrynfisher3700@gmail.com>
2023-10-29 15:10:40 -0400

    added comments into each js file

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
+//Requires fs, specifially writeFile
 const {writeFile} = require('fs/promises')
+//Required for prompt and input recording
 const inquirer = require('inquirer')
+//Required to set maximum length on letter input
 const MaxLengthInputPrompt = require('inquirer-maxlength-input-prompt')
 inquirer.registerPrompt('maxlength-input', MaxLengthInputPrompt)
+//Imported classes 
 const {Circle, Triangle, Square,} = require('./lib/shapes')
 const Logo = require('./lib/logo')
-const { log } = require('console')
 
-
-
-
+//Question prompts to create svg shape, letters, and colors
 inquirer
   .prompt([
     {
@@ -35,10 +36,13 @@ inquirer
     },
   ])
   .then((response) => {
+    //Deconstruction:
     const {letters, colorL, shape, colorS} = response
+    //New instance of Logo class
     let logo = new Logo()
+    //Function to set inputted letters and colors into svg
     logo.setLetters(letters, colorL)
-
+    //New instances of each shape class, based on user input
     let newShape;
     switch(shape) {
       case 'circle':
@@ -50,8 +54,11 @@ inquirer
       case 'square':
         newShape = new Square()
     }
+    //Sets color of new shape instance
     newShape.setColor(colorS)
+    //Set the shape
     logo.setShape(newShape)
+    //Writes the svg file by calling logo.render function
     return writeFile('logo.svg', logo.render(), console.log('Generated logo.svg'))
   })
 
